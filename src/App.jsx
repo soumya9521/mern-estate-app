f3998658697797d4de0a5e0dee4df857fb137837
SOUMYA <soumya9521@gmail.com>
2024-03-11 00:12:59 +0530

    react-router-dom imported and routes added to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import About from "./components/About";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="app text-red-500">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="log-in" element={<Login />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
